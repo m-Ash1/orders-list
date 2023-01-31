36167bba7fbd74186f839fe1b0365b320e600c42
Mahmoud Ashraf <mashraffarouk2702@gmail.com>
2023-01-31 02:19:19 +0200

    app init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// * we write our app here
+// * also we import our components and start building our app with them
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <h1>hello world</h1>
+    </>
+  );
+}
+
+export default App;
